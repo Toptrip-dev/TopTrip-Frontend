0ae75f39fd0f280f6bd835faf0f411cd7fe89197
Omkar Borker <omkarborker15@gmail.com>
2022-11-22 21:32:20 +0530

    new added

--- src/components/pages/map.js
+++ src/components/pages/map.js
@@ -1,120 +1,40 @@
-import React from 'react';
-import { useState } from 'react'; 
-import { GoogleMap, DirectionsRenderer,DirectionsService,Marker,LoadScript,useJsApiLoader } from '@react-google-maps/api';
+import React, { Component } from "react";
+import { Map, TileLayer, withLeaflet, MapControl } from "react-leaflet";
+import MapInfo from "./MapInfo";
+import Routing from "./RoutingMachine";
 
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+class MapComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 17.4,
+      lng: 78.4,
+      zoom: 7,
+      isMapInit: false
+    };
+  }
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+  saveMap = map => {
+    this.map = map;
+    this.setState({
+      isMapInit: true
+    });
+  };
 
-function SimpleMap() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyDA0d4e-jZWyNgpN-vJXeYEIhOspv5SIoQ"
-  })
+  render() {
+    const { lat, lng, zoom } = this.state;
+    const position = [lat, lng];
 
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={{height:'50vh'}}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    return (
+      <Map style={{ height:"100px", position:"absolute"}} center={position} zoom={zoom} ref={this.saveMap}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {this.state.isMapInit && <Routing map={this.map} />}
+      </Map>
+    );
+  }
 }
 
-export default React.memo(SimpleMap)
-// function SimpleMap (props) {
-//   const [response, setResponse] = useState(null);
- 
-//   const hasTwoAddresses = (
-//    props.pickUpAddress !== '' &&
-//    props.dropOffAddress !== ''
-//   );
- 
-//   const directionsCallback = (response) => {
-//    if (response !== null && response.status === 'OK') {
-//     setResponse(response);
-//    }
-//   };
- 
-//   return (
-//    <LoadScript
-//     googleMapsApiKey='AIzaSyAJYHr0VJxCC3QNEmAU5HXDTUP2W9nk5OU'
-//    >
-//     <GoogleMap
-//      center={{
-//       lat: 15.285,
-//       lng:  73.973
-//      }}
-//      mapContainerStyle={{
-//       width: '100%',
-//       height: '300px',
-//       'margin-bottom': '10px'
-//      }}
-//      zoom={props.zoom}
-//     >
-//      {
-//       hasTwoAddresses && (
-//        <DirectionsService
-//         options={{
-//          origin:'Om Sai Tution Classes, Unnamed Road, Swami Vivekanand Society, Borda, Margao, Goa' ,
-//          destination: "Cabo Serai - Eco Resort, Canaguinim, Goa",
-//          travelMode: 'DRIVING'
-//         }}
-//         callback={directionsCallback}
-//        >
-//        </DirectionsService>
-//       )
-//      }
-//      {
-//       hasTwoAddresses && response !== null && (
-//        <DirectionsRenderer
-//         options={{
-//          directions: response
-//         }}
-//        />
-//       )
-//      }
-//      {
-//       !hasTwoAddresses && (
-//        <Marker
-//         label='A'
-//         position={{
-//          lat: 15.285,
-//          lng: 73.973
-//         }}
-//        >
-//        </Marker>
-//       )
-//      }
-//     </GoogleMap>
-//  </LoadScript>
-//   ) ;
-//  }
-
-//  export default SimpleMap;
-
+export default MapComponent;
